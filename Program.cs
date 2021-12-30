55cb9875664b1a8f23fea6aa0edf9f4cc1c3baad
Ingemar Carlsson <ingemar.m.carlsson@outlook.com>
2021-12-30 20:54:06 +0100

    simple poc

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// using Microsoft.Toolkit.Uwp.Notifications;
+using CommunityToolkit.WinUI.Notifications;
+
+new ToastContentBuilder()
+    .AddText("Tjena Berra!")
+    .AddButton(new ToastButton()
+            .SetDismissActivation())
+    .Show();
